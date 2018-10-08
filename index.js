2cbfe1c2dc4b4ba0fbc54d3692974655df2c2030
Chris Cardle <cdcardle@gmail.com>
2018-10-08 03:29:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ function findMatching(drivers, name) {
     return drivers.filter((driver) => driver.toLowerCase() === name.toLowerCase())
 }
 
-// function fuzzymatch(drivers, string) {
-//
-// }
+
 
 functions matchName(drivers, name) {
   return drivers.filter((driver) => driver.name.toLowerCase() === name.toLowerCase())
