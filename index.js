78870dfbd8782d12fdbfdbceae76a57325b98ceb
Chris Cardle <cdcardle@gmail.com>
2018-10-08 03:25:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function findMatching(drivers, string) {
     return drivers.filter(function(driver) {
-      driver.toLowerCase === string.toLowerCase;
+      return driver.toLowerCase === string.toLowerCase;
     })
 }
